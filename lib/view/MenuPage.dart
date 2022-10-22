3184384c78b8b6f9c935ec9416b92fd543ba5c96
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-10-22 15:52:39 +0200

    Created views templates

---
+++ lib/view/MenuPage.dart
@@ -0,0 +1,36 @@
+import 'package:cafe_mobile_app/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 25),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      minimumSize: const Size(300, 40),
+    );
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Menu kawiarni'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Wróć'),
+        ),
+      ),
+    );
+  }
+}
