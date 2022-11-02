403dd62d19fd75cfe33f29ec720c46c92714fb80
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-11-02 21:15:45 +0100

    Created view fot authorized user

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,7 @@
+import 'package:cafe_mobile_app/bloc/auth_bloc/auth.dart';
+import 'package:cafe_mobile_app/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -14,9 +17,19 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: AppColors.darkGoldenrod,
+        title: Text("Witaj w aplikacji!"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
+            },
+            icon: Icon(Icons.logout_outlined))
+        ],
+      ),
       body: Center(
-        child: Stack(),
+        child: Text("Ekran główny po zalogowaniu"),
       ),
     );
   }
