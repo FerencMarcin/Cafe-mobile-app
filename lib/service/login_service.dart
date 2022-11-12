62d15b24ed9ad14a18054fb0a77e5f7d5680443b
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-11-12 02:52:31 +0100

    create login service

---
+++ lib/service/login_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:cafe_mobile_app/model/login_request_model.dart';
+import 'package:cafe_mobile_app/model/login_response_model.dart';
+import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/status/http_status.dart';
+import 'package:http/http.dart' as http;
+
+
+class LoginService extends GetConnect {
+  final String loginUrl = 'http://10.0.2.2:3001/users/login';
+
+  Future<String?> fetchUserLogin(String email, String password) async {
+    LoginRequestModel user = LoginRequestModel(email: email, password: password);
+    var url = Uri.parse(loginUrl);
+
+    http.Response res = await http.post(url, body: user.makeLoginRequest());
+    return res.body;
+    /*
+    if(res.statusCode == HttpStatus.ok) {
+      log('dobrze');
+      final responseData = jsonDecode(res.body);
+      if(responseData['error'] != null) {
+        log('jest blad');
+        return responseData['error'];
+      }
+      LoginResponseModel loggedInUser = LoginResponseModel(roleId: responseData['roleId'], token: responseData['token']);
+      return loggedInUser;
+    } else {
+      log('oj');
+      return jsonDecode(res.body)['error'];
+      return null;
+    }*/
+  }
+}
