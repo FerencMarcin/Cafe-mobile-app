af3204189b129cab5ef1a133a0623a39f9682f4a
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-11-12 02:52:48 +0100

    create login view model

---
+++ lib/viewModel/login_viewModel.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:cafe_mobile_app/model/login_request_model.dart';
+import 'package:cafe_mobile_app/service/auth_service.dart';
+import 'package:cafe_mobile_app/service/login_service.dart';
+import 'package:get/get.dart';
+
+import '../model/login_response_model.dart';
+
+class LoginViewModel extends GetxController {
+  late final LoginService _loginService;
+  late final AuthService _authService;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loginService = Get.put(LoginService());
+    _authService = Get.find();
+  }
+
+  Future<void> userLogin(String email, String password) async {
+    final response = await _loginService.fetchUserLogin(email, password);
+    log('res:  $response');
+
+    if (response != null) {
+      final responseData = jsonDecode(response);
+      if(responseData['error'] != null) {
+        Get.defaultDialog(
+            title: 'Błąd logowania',
+            middleText: responseData['error'],
+            textConfirm: 'Wróć',
+            onConfirm: () {
+              Get.back();
+            }
+        );
+      } else {
+        //LoginResponseModel loggedInUser = LoginResponseModel(roleId: responseData['roleId'], token: responseData['token']);
+        _authService.login(responseData['token']);
+      }
+    } else {
+      Get.defaultDialog(
+        middleText: 'Niepoprawne dane logowania',
+        textConfirm: 'Wróć',
+        onConfirm: () {
+          Get.back();
+        }
+      );
+    }
+  }
+}
