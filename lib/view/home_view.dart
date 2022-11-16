a23d90be020fd6f2451507d13f4725b267bb980c
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-11-16 23:56:49 +0100

    Moved navigation to separate file closes #6

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../theme/colors.dart';
+import 'navigation/navigationDrawer_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -27,70 +28,7 @@ class _HomeViewState extends State<HomeView> {
             icon: Icon(Icons.logout_outlined))
         ],
       ),
-      drawer: Drawer(
-        backgroundColor: AppColors.floralWhite,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: AppColors.darkGoldenrodMap[700],
-              ),
-              child: Text(
-                'Hej ...',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            Divider(
-              color: Colors.black54,
-            ),
-            ListTile(
-              leading: Icon(Icons.home_outlined),
-              title: Text('Strona główna'),
-            ),
-            ListTile(
-              leading: Icon(Icons.info_outline_rounded),
-              title: Text('O nas'),
-            ),
-            ListTile(
-              leading: Icon(Icons.menu_book_outlined),
-              title: Text('Menu'),
-            ),
-            ListTile(
-              leading: Icon(Icons.local_offer_outlined),
-              title: Text('Oferty specjalne'),
-            ),
-            Divider(
-              color: Colors.black54,
-            ),
-            ListTile(
-              leading: Icon(Icons.list_alt_outlined),
-              title: Text('Moje zamówienia'),
-            ),
-            ListTile(
-              leading: Icon(Icons.qr_code_2_outlined),
-              title: Text('Talony'),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 50),
-              child: Divider(
-                color: Colors.black54,
-              )
-            ),
-            ListTile(
-              leading: Icon(Icons.person_outline_rounded),
-              title: Text('Moje konto'),
-            ),
-            ListTile(
-              leading: Icon(Icons.logout_outlined),
-              title: Text('Wyloguj się'),
-            ),
-          ],
-        ),
-      ),
+      drawer: NavigationDrawer(),
       body: Center(
         child: Text('Ekran głowny')
       ),
---
+++ lib/view/navigation/navigationDrawer_view.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../service/auth_service.dart';
+import '../../theme/colors.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  NavigationDrawer({Key? key}) : super(key: key);
+  final AuthService _authManager = Get.find();
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+    backgroundColor: AppColors.floralWhite,
+    child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          navHeader(context),
+          navItems(context),
+        ],
+      ),
+    ),
+  );
+
+  Widget navHeader(BuildContext context) => Container(
+    color: AppColors.darkGoldenrodMap[700],
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top
+      ),
+      child: Column(
+        children: const [
+          Icon(
+            Icons.account_circle_outlined,
+            size: 80,
+            color: AppColors.floralWhite,
+          ),
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "imie i nazwisko",
+              style: TextStyle(fontSize: 20, color: AppColors.floralWhite),
+            ),
+          ),
+        ],
+      )
+  );
+
+  Widget navItems(BuildContext context) => Wrap(
+    runSpacing: 10,
+    children: [
+      const ListTile(
+        leading: Icon(Icons.home_outlined),
+        title: Text('Strona główna'),
+      ),
+      const ListTile(
+        leading: Icon(Icons.info_outline_rounded),
+        title: Text('O nas'),
+      ),
+      const ListTile(
+        leading: Icon(Icons.menu_book_outlined),
+        title: Text('Menu'),
+      ),
+      const ListTile(
+        leading: Icon(Icons.local_offer_outlined),
+        title: Text('Oferty specjalne'),
+      ),
+      const Divider(
+        color: Colors.black54,
+      ),
+      const ListTile(
+        leading: Icon(Icons.list_alt_outlined),
+        title: Text('Moje zamówienia'),
+      ),
+      const ListTile(
+        leading: Icon(Icons.qr_code_2_outlined),
+        title: Text('Talony'),
+      ),
+      const Padding(
+          padding: EdgeInsets.only(bottom: 50),
+          child: Divider(
+            color: Colors.black54,
+          )
+      ),
+      const ListTile(
+        leading: Icon(Icons.person_outline_rounded),
+        title: Text('Moje konto'),
+      ),
+      ListTile(
+        leading: const Icon(Icons.logout_outlined),
+        title: const Text('Wyloguj się'),
+        onTap: () => _authManager.logout(),
+      ),
+    ],
+  );
+}
