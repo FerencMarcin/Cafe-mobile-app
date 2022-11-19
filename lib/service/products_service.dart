87f395979ec57f2167f5628afb338648df55fbc4
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-11-19 23:51:33 +0100

    Create getProducts form server method #7

--- lib/service/products_service.dart
+++ lib/service/products_service.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:cafe_mobile_app/model/category_model.dart';
 import 'package:cafe_mobile_app/model/login_request_model.dart';
 import 'package:cafe_mobile_app/model/login_response_model.dart';
+import 'package:cafe_mobile_app/model/product_model.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/status/http_status.dart';
 import 'package:http/http.dart' as http;
@@ -34,7 +35,7 @@ class ProductsService extends GetConnect {
       return null;
     }*/
   }
-
+  //Todo pozbyć się _service - metode do view model
   Future<List<CategoryModel>> getCategories() async {
     log('1');
     List<CategoryModel> categories = <CategoryModel>[];
@@ -51,5 +52,32 @@ class ProductsService extends GetConnect {
     });
 
     return categories;
+  }
+
+  Future<List<ProductModel>> getProducts() async {
+    log('2');
+    List<ProductModel> products = <ProductModel>[];
+    var urlActiveProducts = Uri.parse(getActiveProductsUrl);
+    http.Response response = await http.get(urlActiveProducts);
+
+    var data = jsonDecode(response.body);
+
+    ProductModel product;
+    data.forEach((newProduct) => {
+      product = ProductModel(
+          newProduct['id'],
+          newProduct['name'],
+          newProduct['size'],
+          newProduct['price'],
+          newProduct['allergen'],
+          newProduct['createdAt'],
+          newProduct['updatedAt'],
+          newProduct['categoryId'],
+          newProduct['statusId']),
+      products.add(product),
+      log(product.toString())
+    });
+
+    return products;
   }
 }
