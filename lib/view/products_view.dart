d661b6fe6ee32aba651d2f188ad9247299d6fa94
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-11-19 23:51:03 +0100

    Product category view fixed #7

--- lib/view/products_view.dart
+++ lib/view/products_view.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:cafe_mobile_app/service/products_service.dart';
+import 'package:cafe_mobile_app/theme/colors.dart';
 import 'package:cafe_mobile_app/viewModel/products_viewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,22 +20,21 @@ class _ProductsViewState extends State<ProductsView> {
   ProductsViewModel _productsViewModel = Get.put(ProductsViewModel());
   ProductsService _productsService = Get.put(ProductsService());
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarView(appBarTitle: 'Menu'),
       body: Column(
         children: [
-          Scrollbar(
-              child: TextButton(
-            child: Text('tu będą kategorie'),
-            onPressed: () {
-              _productsViewModel.getCategories(context);
-            },
-          )),
+          sectionTitle('Kategorie'),
+
           //TODO it works fix service and viewmodel dependency
 
-          FutureBuilder(
+          Container(
+            height: 50,
+            child: FutureBuilder(
               future: _productsService.getCategories(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState != ConnectionState.done) {
@@ -47,20 +47,40 @@ class _ProductsViewState extends State<ProductsView> {
                 }
                 List<CategoryModel> categories = snapshot.data ?? [];
                 return ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: categories.length,
-                    itemBuilder: (context, index) {
-                      CategoryModel category = categories[index];
-                      return ListTile(
-                        title: Text(category.name!),
-                      );
-                    });
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    CategoryModel category = categories[index];
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(150, 5),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                          )
+                        ),
+                        child: Text(
+                          category.name!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)
+                        ),
+                        onPressed: () {  },
+                      ),
+                    );
+                  });
               })
-
-
+          )
         ],
       ),
+    );
+  }
+
+  Padding sectionTitle(String title) {
+    return Padding(
+        padding: EdgeInsets.all(15),
+        child: Text(title, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800, color: AppColors.darkGoldenrod))
     );
   }
 }
