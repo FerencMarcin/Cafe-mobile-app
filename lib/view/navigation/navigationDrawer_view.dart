3a67878938abc9d01417016cc302c92944782e72
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-21 21:01:04 +0100

    Code re factorization #12

--- lib/view/navigation/navigationDrawer_view.dart
+++ lib/view/navigation/navigationDrawer_view.dart
@@ -43,8 +43,7 @@ class NavigationDrawer extends StatelessWidget {
         children: [
           const Icon(
             Icons.account_circle_outlined,
-            size: 80,
-            color: AppColors.floralWhite,
+            size: 80, color: AppColors.floralWhite,
           ),
           Padding(
             padding: const EdgeInsets.all(15.0),
@@ -57,14 +56,12 @@ class NavigationDrawer extends StatelessWidget {
                       style: const TextStyle(fontSize: 20.0, color: AppColors.floralWhite),
                     );
                   } else {
-                    return const Text(
-                      'Ładowanie...',
+                    return const Text('Ładowanie...',
                       style: TextStyle(fontSize: 20.0, color: AppColors.floralWhite),
                     );
                   }
                 }
             )
-
           ),
         ],
       )
@@ -74,10 +71,7 @@ class NavigationDrawer extends StatelessWidget {
   ListTile sideMenuItem(String route, String label, Icon labelIcon, BuildContext context) {
     return ListTile(
       leading: labelIcon,
-      title:
-      (Get.currentRoute == route) ?
-      Text(label, style: selectedRouteStyle)
-          : Text(label, style: unselectedRouteStyle),
+      title: Text(label, style: unselectedRouteStyle),
       onTap: () => Navigator.pushNamed(context, route),
     );
   }
@@ -100,50 +94,14 @@ class NavigationDrawer extends StatelessWidget {
           }
         }
       ),
-      ListTile(
-        leading: const Icon(Icons.info_outline_rounded),
-        title:
-          (Get.currentRoute == '/aboutus') ?
-          Text('O nas', style: selectedRouteStyle)
-          : Text('O nas', style: unselectedRouteStyle),
-      ),
+      sideMenuItem('/aboutUs', 'O nas', const Icon(Icons.info_outline_rounded), context),
       sideMenuItem('/menu', 'Menu', const Icon(Icons.menu_book_outlined), context),
-
-      ListTile(
-        leading: const Icon(Icons.local_offer_outlined),
-        title:
-          (Get.currentRoute == '/specialoffers') ?
-          Text('Oferty specjalne', style: selectedRouteStyle)
-          : Text('Oferty specjalne', style: unselectedRouteStyle),
-      ),
-      const Divider(
-        color: Colors.black54,
-      ),
-      ListTile(
-        leading: const Icon(Icons.list_alt_outlined),
-        title:
-          (Get.currentRoute == '/myorders') ?
-          Text('Moje zamówienia', style: selectedRouteStyle)
-          : Text('Moje zamówienia', style: unselectedRouteStyle),
-      ),
-      ListTile(
-        leading: const Icon(Icons.qr_code_2_outlined),
-        title:
-          (Get.currentRoute == '/codes') ?
-          Text('Talony', style: selectedRouteStyle)
-          : Text('Talony', style: unselectedRouteStyle),
-      ),
-      const Padding(
-          padding: EdgeInsets.only(bottom: 50.0),
-          child: Divider(color: Colors.black54)
-      ),
-      ListTile(
-        leading: const Icon(Icons.person_outline_rounded),
-        title:
-          (Get.currentRoute == '/profile') ?
-          Text('Moje konto', style: selectedRouteStyle)
-          : Text('Moje konto', style: unselectedRouteStyle),
-      ),
+      sideMenuItem('/vouchers', 'Talony', const Icon(Icons.local_offer_outlined), context),
+      const Divider(color: Colors.black54),
+      sideMenuItem('/userOrders', 'Moje zamówienia', const Icon(Icons.list_alt_outlined), context),
+      sideMenuItem('/userVouchers', 'Moje talony', const Icon(Icons.qr_code_2_outlined), context),
+      const Divider(color: Colors.black54),
+      sideMenuItem('/userProfile', 'Moje konto', const Icon(Icons.person_outline_rounded), context),
       ListTile(
         leading: const Icon(Icons.logout_outlined, size: 32.0),
         title: Text('Wyloguj się', style: unselectedRouteStyle),
