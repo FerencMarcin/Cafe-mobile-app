b2996a2fdb3741a1b0fb090e2f9d521c3f614363
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-21 18:39:05 +0100

    Add null check and get user info method #15

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -2,13 +2,20 @@ import 'dart:developer';
 
 import 'package:cafe_mobile_app/service/storage_service.dart';
 import 'package:get/get.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+import 'interceptor/dioClient.dart';
 
 class AuthService extends GetxController with StorageService {
   final isLogged = false.obs;
+  DioClient _dioClient = Get.put(DioClient());
 
   void login(String? token) async {
-    isLogged.value = true;
-    await setAccessToken(token!);
+    if(token != null) {
+      isLogged.value = true;
+      await setAccessToken(token!);
+      await saveUserInfo(token);
+    }
   }
 
   void addRefreshToken(String token) async {
@@ -44,5 +51,14 @@ class AuthService extends GetxController with StorageService {
     } catch(e) {
       isLogged.value = false;
     }
+  }
+
+  Future<void> saveUserInfo(String token) async {
+    Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+    //decodedToken.forEach((key, value) {log("key val: "+ key.toString() + " - " + value.toString());});
+    log("email: " + decodedToken['user']['email'].toString());
+    String userEmail = decodedToken['user']['email'];
+    final result = await _dioClient.dioClient.get('http://10.0.2.2:3001/users/email/' + userEmail);
+    log("res: " + result.toString());
   }
 }
