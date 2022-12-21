a54513674fbe63212b1cd2eff4b9120c45229382
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-21 19:57:07 +0100

    Add dependency

--- lib/view/startViewManager.dart
+++ lib/view/startViewManager.dart
@@ -1,5 +1,6 @@
 import 'package:cafe_mobile_app/service/auth_service.dart';
 import 'package:cafe_mobile_app/view/welcome_view.dart';
+import 'package:cafe_mobile_app/viewModel/login_viewModel.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,10 +10,11 @@ import 'auth_view.dart';
 class StartView extends StatelessWidget {
   const StartView({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     AuthService _authManager = Get.find();
-
+    LoginViewModel loginViewModel = Get.put(LoginViewModel());
     return Obx(() {
       if (_authManager.isLogged.value) {
         return HomePageView();
