9524902f899974f05661e065a2742f26157fab22
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-22 19:21:01 +0100

    Disable android back button on home screen #12

--- lib/view/homePage_view.dart
+++ lib/view/homePage_view.dart
@@ -19,29 +19,36 @@ class _HomePageViewState extends State<HomePageView> {
   AuthService _authManager = Get.find();
   //DioClient _test = Get.put(DioClient());
 
+  Future<bool> _onWillPop() async {
+    return false; //<-- SEE HERE
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarView(appBarTitle: 'Strona główna'),
-      drawer: NavigationDrawer(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            String? token = await _authManager.getAccessToken();
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBarView(appBarTitle: 'Strona główna'),
+        drawer: NavigationDrawer(),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              String? token = await _authManager.getAccessToken();
 
-            if(token != null){
-              //log("tokennn: " + token);
-              Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
-              //decodedToken.forEach((key, value) {log("key val: "+ key.toString() + " - " + value.toString());});
-              //log("email: " + decodedToken['user']['email'].toString());
-              String userEmail = decodedToken['user']['email'];
-              _authManager.fetchUserInfo(token);
+              if(token != null){
+                //log("tokennn: " + token);
+                Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+                //decodedToken.forEach((key, value) {log("key val: "+ key.toString() + " - " + value.toString());});
+                //log("email: " + decodedToken['user']['email'].toString());
+                String userEmail = decodedToken['user']['email'];
+                _authManager.fetchUserInfo(token);
 
-            }
+              }
 
-          //TODO refactor
-          log("clicked");
-        }, child: Text("ekran głowny"),)
+            //TODO refactor
+            log("clicked");
+          }, child: Text("ekran głowny"),)
+        ),
       ),
     );
   }
