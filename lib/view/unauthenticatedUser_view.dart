4fc2725606fc7cc26c0baa7597a7f458c6128d69
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-23 00:15:46 +0100

    Finish unauth user view closes #23

--- lib/view/unauthenticatedUser_view.dart
+++ lib/view/unauthenticatedUser_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../theme/colors.dart';
@@ -11,54 +12,123 @@ class UnauthenticatedUserView extends StatefulWidget {
 }
 
 class _UnauthenticatedUserViewState extends State<UnauthenticatedUserView> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: AppColors.floralWhite,
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Stack(
-                children: [
-                  ClipPath(
-                    clipper:ImageBorderClip(), //set our custom wave clipper
-                    child:Container(
-                      color: AppColors.burlyWood,
-                      height:220.0,
+      body: SingleChildScrollView(
+        child: Container(
+          height: 900.0,
+          color: AppColors.floralWhite,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Stack(
+                  children: [
+                    ClipPath(
+                      clipper:ImageBorderClip(), //set our custom wave clipper
+                      child:Container(
+                        color: AppColors.burlyWood,
+                        height:220.0,
+                      ),
                     ),
-                  ),
-                  ClipPath(
-                    clipper: ImageBorderClip(),
-                    child: Container(
-                      height: 200.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        image: DecorationImage(
-                            image: const AssetImage("images/unauthHeader.jpg"),
-                            colorFilter: ColorFilter.mode(AppColors.floralWhite.withOpacity(0.6), BlendMode.modulate),
-                            fit: BoxFit.cover
+                    ClipPath(
+                      clipper: ImageBorderClip(),
+                      child: Container(
+                        height: 200.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: DecorationImage(
+                              image: const AssetImage("images/unauthHeader.jpg"),
+                              colorFilter: ColorFilter.mode(AppColors.floralWhite.withOpacity(0.6), BlendMode.modulate),
+                              fit: BoxFit.cover
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 30.0),
-                    child: homePageMenuButton(outlineButtonStyle, '/login', 'Ekran główny'),
-                  ),
-                  homePageMenuButton(outlineButtonStyle, '/login', 'Zaloguj się'),
-                ],
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                          CarouselSlider(
+                            items: [
+                              carouselItem('images/sliderMenu.jpg', 'Menu kawiarnii', '/menu'),
+                              carouselItem('images/sliderInfo.jpg', 'O nas', '/aboutUs'),
+                              carouselItem('images/sliderLogin.jpg', 'Dołącz do nas!', '/login')
+                            ],
+                            options: CarouselOptions(
+                              height: 350.0,
+                              enlargeCenterPage: true,
+                              autoPlay: true,
+                              autoPlayCurve: Curves.fastOutSlowIn,
+                              enableInfiniteScroll: true,
+                              autoPlayAnimationDuration: Duration(milliseconds: 800),
+                              viewportFraction: 0.8,
+                      )
+                    ),
+
+                    Container(
+                      alignment: Alignment.topCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25.0),
+                        child: Text("Dlaczego warto dołączyć?", style: TextStyle(color: AppColors.darkGoldenrodMap[800], fontSize: 24.0, fontWeight: FontWeight.w600),),
+                      ),
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15.0, bottom: 25.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(" - Możliwość rezerwacji stolika", style: benefitTextStyle),
+                            Text(" - Zbieranie punktów lojalnościowych", style: benefitTextStyle),
+                            Text(" - Atrakcyjne kupony promocyjne", style: benefitTextStyle),
+                            Text(" - Podgląd historii zamówień", style: benefitTextStyle)
+                          ],
+                        ),
+                      ),
+                    ),
+                    homePageMenuButton(outlineButtonStyle, '/login', 'Zaloguj się'),
+                  ],
+                )
               )
-            )
-          ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  GestureDetector carouselItem(String photoPath, String label, String route) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, route);
+      },
+      child: Container(
+        width: 300.0,
+        alignment: Alignment.bottomCenter,
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          image: DecorationImage(
+            image: AssetImage(photoPath),
+            colorFilter: ColorFilter.mode(AppColors.burlyWood.withOpacity(0.9), BlendMode.modulate),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(label, style: const TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w600,
+              color: AppColors.floralWhite),
+          ),
         ),
       ),
     );
@@ -71,6 +141,12 @@ class _UnauthenticatedUserViewState extends State<UnauthenticatedUserView> {
       backgroundColor: AppColors.darkGoldenrodMap[50],
       elevation: 10.0,
       shadowColor: AppColors.darkGoldenrodMap[100]
+  );
+
+  final TextStyle benefitTextStyle = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.darkGoldenrodMap[700]
   );
 
   final TextStyle titleTextStyle = TextStyle(
