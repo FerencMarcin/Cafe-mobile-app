aeb3b91be5ea8e16a624117df56afd2dbc7115e4
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-25 04:19:50 +0100

    Code clean #12

--- lib/view/components/sectionTitle.dart
+++ lib/view/components/sectionTitle.dart
@@ -5,6 +5,9 @@ import '../../theme/colors.dart';
 Padding sectionTitle(String title) {
   return Padding(
       padding: const EdgeInsets.all(15),
-      child: Text(title, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800, color: AppColors.darkGoldenrodMap[900]))
+      child: Text(title, style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.w800,
+          color: AppColors.darkGoldenrodMap[900]))
   );
 }
