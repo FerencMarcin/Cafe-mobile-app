7dc2e75f3e0e3fd4468676f7a51b00ce0a11d3b8
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-29 20:45:18 +0100

    Change hardcoded url into path using .env variable #12

--- lib/viewModel/auth_viewModel.dart
+++ lib/viewModel/auth_viewModel.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:cafe_mobile_app/service/auth_service.dart';
-import 'package:cafe_mobile_app/service/login_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../model/login_request_model.dart';
 import '../model/registration_request_model.dart';
@@ -13,10 +10,12 @@ import '../model/registration_request_model.dart';
 class AuthViewModel extends GetxController {
   final AuthService _authService = Get.find();
 
-  final String loginUrl = 'http://10.0.2.2:3001/users/login';
+  final String loginUrl = '${dotenv.env['BASE_URL']!}/users/login';
+  final String registerUrl = '${dotenv.env['BASE_URL']!}/users/register';
+  final String resetPasswordRequestUrl = '${dotenv.env['BASE_URL']!}/auth/requestpasswordreset';
+  final String resetPasswordUrl = '${dotenv.env['BASE_URL']!}/auth/resetpassword';
 
   Future<void> userLogin(String email, String password) async {
-    log('baseutl: ' + dotenv.env['BASE_URL']!);
     LoginRequestModel user = LoginRequestModel(email: email, password: password);
     var url = Uri.parse(loginUrl);
 
@@ -29,8 +28,6 @@ class AuthViewModel extends GetxController {
       _authService.login(responseData['accessToken']);
     }
   }
-
-  final String registerUrl = 'http://10.0.2.2:3001/users/register';
 
   Future<String> userRegistration(String email, String password, String firstName, String lastName, String number, String sex) async {
     RegistrationRequestModel newUser = RegistrationRequestModel(
@@ -45,7 +42,6 @@ class AuthViewModel extends GetxController {
     final http.Response response = await http.post(url, body: newUser.makeRegistrationRequest());
     final responseData = jsonDecode(response.body);
     if(response.statusCode != 200) {
-      log(responseData.toString());
       throw responseData['error'];
     } else {
       return 'Konto zostało utworzone pomyślnie';
@@ -53,16 +49,13 @@ class AuthViewModel extends GetxController {
   }
 
   Future<dynamic> resetPassword(String email) async {
-    const String resetPasswordUrl = 'http://10.0.2.2:3001/auth/requestpasswordreset';
-    var url = Uri.parse(resetPasswordUrl);
+    var url = Uri.parse(resetPasswordRequestUrl);
     final http.Response response = await http.post(url, body: {"email": email});
-    log(response.body.toString());
     final responseData = jsonDecode(response.body);
     return responseData;
   }
 
   Future<String> changePassword(String resetCode, String email, String password) async {
-    const String resetPasswordUrl = 'http://10.0.2.2:3001/auth//resetpassword';
     var url = Uri.parse(resetPasswordUrl);
     final http.Response response = await http.post(url,
       body: {
@@ -71,7 +64,6 @@ class AuthViewModel extends GetxController {
         "resetCode": resetCode
       }
     );
-    log(response.body.toString());
     final responseData = jsonDecode(response.body);
     if (responseData['error'] == null ) {
       return responseData['message'];
