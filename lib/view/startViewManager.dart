c657d0cec847fcee889010299ad89cbed27bf859
MarcinFerenc <ferenc.marcin0414@gmail.com>
2022-12-29 21:44:25 +0100

    auth view model get.find error fix #12

--- lib/view/startViewManager.dart
+++ lib/view/startViewManager.dart
@@ -3,6 +3,7 @@ import 'package:cafe_mobile_app/view/welcome_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../viewModel/auth_viewModel.dart';
 import 'homePage_view.dart';
 import 'login_view.dart';
 
@@ -12,6 +13,7 @@ class StartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AuthService _authManager = Get.find();
+    final AuthViewModel _authViewModel = Get.put(AuthViewModel());
     return Obx(() {
       if (_authManager.isLogged.value) {
         return HomePageView();
