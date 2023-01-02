f40c5993392d5abb4ab9b5e35aabdebf9e46ef6f
MarcinFerenc <ferenc.marcin0414@gmail.com>
2023-01-02 23:37:10 +0100

    Add styling #12

--- lib/view/resetPassword_view.dart
+++ lib/view/resetPassword_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../theme/colors.dart';
 import '../viewModel/auth_viewModel.dart';
+import 'clipper/imageBorderClip.dart';
 import 'components/sectionTitle.dart';
+import 'components/authForm.dart';
 
 class ResetPasswordView extends StatefulWidget {
   const ResetPasswordView({Key? key}) : super(key: key);
@@ -24,49 +27,71 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
+      body: SingleChildScrollView(
         child: Form(
           autovalidateMode: AutovalidateMode.always,
           key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              sectionTitle('Resetowanie hasła'),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text('Podaj adres e-mail, powiązany z kontem, dla którego chcesz zresetować hasło. Wyślemy na niego kod resetujący.'),
+              Stack(
+                children: [
+                  ClipPath(
+                    clipper: ImageBorderClip(), //set our custom wave clipper
+                    child:Container(
+                      color: AppColors.burlyWood,
+                      height: 300.0,
+                    ),
+                  ),
+                  ClipPath(
+                    clipper: ImageBorderClip(),
+                    child: Container(
+                      height: 280.0,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(
+                            image: AssetImage("images/initPageImage.jpg"),
+                            colorFilter: ColorFilter.mode(AppColors.photoFilter, BlendMode.modulate),
+                            fit: BoxFit.cover
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Adres email',
-                ),
-                validator: (value) {
-                  return (value == null || value.isEmpty)
-                      ? 'Należy podać adres email'
-                      : null;
-                },
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0, bottom: 10.0),
+                child: sectionTitle('Resetowanie hasła')
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text('Podaj adres e-mail, powiązany z kontem, dla którego chcesz zresetować hasło. Wyślemy na niego kod resetujący.', style: textStyle),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(35.0, 10.0, 35.0, 25.0),
+                child: AuthForm.emailFormField(emailController),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
-                      onPressed: ()  {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Anuluj')
+                    style: buttonStyle,
+                    onPressed: ()  {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Anuluj', style: buttonTextStyle)
                   ),
-                  SizedBox(width: 20.0),
+                  const SizedBox(width: 20.0),
                   ElevatedButton(
-                      onPressed: () async {
-                        if (_formKey.currentState?.validate() ?? false) {
+                    style: buttonStyle,
+                    onPressed: () async {
+                      if (_formKey.currentState?.validate() ?? false) {
 
-                          final message = await _authViewModel.resetPassword(emailController.text.trim());
-                          Get.dialog(passwordResetStatusMessage(message));
-                        }
-                      },
-                      child: Text('Wyślij kod')
+                        final message = await _authViewModel.resetPassword(emailController.text.trim());
+                        Get.dialog(passwordResetStatusMessage(message));
+                      }
+                    },
+                    child: Text('Wyślij kod', style: buttonTextStyle)
                   ),
                 ],
               )
@@ -88,8 +113,11 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
     return StatefulBuilder(
         builder: (context, setState) {
           return AlertDialog(
-            title: const Text("Resetowanie hasła"),
-            content: Text(content),
+            title: Center(child: sectionTitle("Resetowanie hasła")),
+            content: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(content, textAlign: TextAlign.justify, style: textStyle),
+            ),
             actions: message['error'] == null
                 ? <Widget>[
                   Form(
@@ -97,33 +125,40 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
                     key: _newPassFormKey,
                     child: Column(
                       children: [
-                        TextFormField(
-                          controller: resetCodeController,
-                          decoration: const InputDecoration(
-                            labelText: 'Kod resetujący',
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                          child: TextFormField(
+                            controller: resetCodeController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Kod resetujący',
+                              labelStyle: TextStyle(
+                                  color: AppColors.darkGoldenrodMap[800]
+                              ),
+                              prefixIcon: Icon(Icons.wifi_protected_setup_outlined, color: AppColors.darkGoldenrodMap[400]),
+                            ),
+                            validator: (value) {
+                              return (value == null || value.isEmpty)
+                                  ? 'Należy podać kod z wiadomości email'
+                                  : null;
+                            },
                           ),
-                          validator: (value) {
-                            return (value == null || value.isEmpty)
-                                ? 'Należy podać kod z wiadomości email'
-                                : null;
-                          },
                         ),
-                        TextFormField(
-                          controller: newPassController,
-                          decoration: const InputDecoration(
-                            labelText: 'Nowe hasło',
-                          ),
-                          validator: (value) {
-                            return (value == null || value.isEmpty)
-                                ? 'Należy podać hasło'
-                                : null;
-                          },
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 15.0),
+                          child: AuthForm.passwordFormFieldValidated(newPassController),
                         ),
                         closeButton ? ElevatedButton(
-                          child: const Text('Zamknij'),
-                          onPressed: () { Navigator.pop((context));},
+                          style: buttonStyle,
+                          child: Text('Zamknij', style: buttonTextStyle,),
+                          onPressed: () {
+                            resetCodeController.clear();
+                            newPassController.clear();
+                            Navigator.pop((context));
+                          },
                         ) : ElevatedButton(
-                          child: const Text('Zmień hasło'),
+                          style: buttonStyle,
+                          child: Text('Zmień hasło', style: buttonTextStyle,),
                           onPressed: () async {
                             if (_newPassFormKey.currentState?.validate() ?? false) {
                               String response = await _authViewModel.changePassword(
@@ -143,7 +178,8 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
                   )
                 ] : <Widget>[
                   ElevatedButton(
-                    child: const Text('Wróć'),
+                    style: buttonStyle,
+                    child: Text('Wróć', style: buttonTextStyle,),
                     onPressed: () { Navigator.pop((context));},
                   ),
                 ]
@@ -151,4 +187,24 @@ class _ResetPasswordViewState extends State<ResetPasswordView> {
         }
     );
   }
+
+  final TextStyle textStyle = TextStyle(
+      color: AppColors.darkGoldenrodMap[900],
+      fontSize: 17.0
+  );
+
+  final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+      minimumSize: const Size(100.0, 40.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      side: const BorderSide(width: 2.0, color: AppColors.aztecGold),
+      backgroundColor: AppColors.darkGoldenrodMap[50],
+      elevation: 5.0,
+      shadowColor: AppColors.darkGoldenrodMap[100]
+  );
+
+  final TextStyle buttonTextStyle = TextStyle(
+      color: AppColors.darkGoldenrodMap[900],
+      fontWeight: FontWeight.bold,
+      fontSize: 17.0
+  );
 }
