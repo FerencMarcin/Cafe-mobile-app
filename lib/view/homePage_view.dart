07892bbbf61b08173649ec611fcffe32fc257fcb
MarcinFerenc <ferenc.marcin0414@gmail.com>
2023-01-10 20:55:37 +0100

    Upgrade homepage view #12

--- lib/view/homePage_view.dart
+++ lib/view/homePage_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../theme/colors.dart';
 import 'appBar/appBar_view.dart';
+import 'clipper/imageBorderClip.dart';
 import 'components/sectionTitle.dart';
 import 'navigation/navigationDrawer_view.dart';
 
@@ -26,47 +27,121 @@ class _HomePageViewState extends State<HomePageView> {
         appBar: AppBarView(appBarTitle: 'Strona główna'),
         drawer: NavigationDrawer(),
         body: SingleChildScrollView(
-          child: SizedBox(
-            height: 900.0,
-            child: Column(
-              children: [
-                Expanded(
-                    flex: 3,
-                    child: Column(
-                      children: [
-                        CarouselSlider(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  ClipPath(
+                    clipper: ImageBorderClip(), //set our custom wave clipper
+                    child: Container(
+                      color: AppColors.burlyWood,
+                      height: 200.0,
+                    ),
+                  ),
+                  ClipPath(
+                    clipper: ImageBorderClip(),
+                    child: Container(
+                      height: 190.0,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(
+                            image: AssetImage("images/initPageImage.jpg"),
+                            colorFilter: ColorFilter.mode(
+                                AppColors.photoFilter, BlendMode.modulate),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 40.0),
+                      child: Row(
+                        children: [
+                          Spacer(),
+                          Text('Witaj! Czego dzisiaj się napijesz?',
+                              style: titleTextStyle),
+                          Spacer()
+                        ],
+                      )),
+                ],
+              ),
+              SizedBox(
+                  height: 160,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    padding: const EdgeInsets.all(8),
+                    children: <Widget>[
+                      listItem("images/coffeeIcon1.jpg"),
+                      listItem("images/coffeeIcon2.jpg"),
+                      listItem("images/coffeeIcon3.jpg"),
+                      listItem("images/coffeeIcon4.jpg"),
+                      listItem("images/coffeeIcon5.jpg"),
+                    ],
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          top: BorderSide(color: AppColors.aztecGold, width: 5.0)
+                      )
+                  ),
+                  child: sectionTitle('Zobacz co dla Ciebie mamy:'),
+                ),
+              ),
+              SizedBox(
+                  height: 500.0,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: CarouselSlider(
                             items: [
-                              carouselItem('images/mainPage1.jpg', 'Menu kawiarnii', '/menu', Icons.coffee_outlined),
-                              carouselItem('images/mainPage5.jpg', 'O nas', '/aboutUs', Icons.coffee_maker_outlined),
-                              carouselItem('images/mainPage4.jpg', 'Oferty specjalne!', '/vouchers', Icons.price_check_outlined),
-                              carouselItem('images/mainPage3.jpg', 'Planujesz wizytę?', '/newReservation', Icons.calendar_month_outlined),
-                              carouselItem('images/mainPage2.jpg', 'Twoja historia', '/userOrders', Icons.history_outlined)
+                              carouselItem(
+                                  'images/mainPage1.jpg',
+                                  'Menu kawiarnii',
+                                  '/menu',
+                                  Icons.coffee_outlined),
+                              carouselItem('images/mainPage5.jpg', 'O nas',
+                                  '/aboutUs', Icons.coffee_maker_outlined),
+                              carouselItem(
+                                  'images/mainPage4.jpg',
+                                  'Oferty specjalne!',
+                                  '/vouchers',
+                                  Icons.price_check_outlined),
+                              carouselItem(
+                                  'images/mainPage3.jpg',
+                                  'Planujesz wizytę?',
+                                  '/newReservation',
+                                  Icons.calendar_month_outlined),
+                              carouselItem(
+                                  'images/mainPage2.jpg',
+                                  'Twoja historia',
+                                  '/userOrders',
+                                  Icons.history_outlined)
                             ],
                             options: CarouselOptions(
-                              height: 500.0,
-                              viewportFraction: 1.2,
+                              height: 450.0,
+                              viewportFraction: 0.9,
                               enlargeCenterPage: true,
                               autoPlay: true,
                               autoPlayCurve: Curves.fastOutSlowIn,
                               enableInfiniteScroll: true,
-                              autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                            )
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10.0),
-                          child: sectionTitle('Witaj!'))
-                      ],
-                    )
-                )
-              ],
-            ),
+                              autoPlayAnimationDuration:
+                                  const Duration(milliseconds: 800),
+                            )),
+                      ),
+                    ],
+                  ))
+            ],
           ),
         ),
       ),
     );
   }
 
-  GestureDetector carouselItem(String photoPath, String label, String route, IconData icon) {
+  GestureDetector carouselItem(
+      String photoPath, String label, String route, IconData icon) {
     return GestureDetector(
       onTap: () {
         Navigator.pushNamed(context, route);
@@ -75,9 +150,11 @@ class _HomePageViewState extends State<HomePageView> {
         width: 400.0,
         alignment: Alignment.bottomCenter,
         decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
           image: DecorationImage(
             image: AssetImage(photoPath),
-            colorFilter: const ColorFilter.mode(AppColors.photoFilter, BlendMode.modulate),
+            colorFilter: const ColorFilter.mode(
+                AppColors.photoFilter, BlendMode.modulate),
             fit: BoxFit.cover,
           ),
         ),
@@ -88,14 +165,16 @@ class _HomePageViewState extends State<HomePageView> {
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 20.0),
-                child: Icon(icon, size: 200.0, color: AppColors.aztecGold.withOpacity(0.3)),
+                child: Icon(icon,
+                    size: 200.0, color: AppColors.aztecGold.withOpacity(0.3)),
               ),
-              Text(label, style: const TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.floralWhite),
+              Text(
+                label,
+                style: const TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.floralWhite),
               ),
-
             ],
           ),
         ),
@@ -103,17 +182,42 @@ class _HomePageViewState extends State<HomePageView> {
     );
   }
 
-  OutlinedButton homePageMenuButton(ButtonStyle style, String path, String label) {
+  Padding listItem(String imagePath) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/menu');
+        },
+        child: Container(
+          width: 150.0,
+          height: 150.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+            image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.fill),
+          ),
+        ),
+      ),
+    );
+  }
+
+  OutlinedButton homePageMenuButton(
+      ButtonStyle style, String path, String label) {
     return OutlinedButton(
       style: style,
-      onPressed: () {Navigator.pushNamed(context, path);},
-      child: Padding(padding: const EdgeInsets.all(10.0),
+      onPressed: () {
+        Navigator.pushNamed(context, path);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Text(label,
             style: TextStyle(
                 fontSize: 22.0,
                 fontWeight: FontWeight.w500,
-                color: AppColors.darkGoldenrodMap[800])
-        ),
+                color: AppColors.darkGoldenrodMap[800])),
       ),
     );
   }
@@ -124,12 +228,15 @@ class _HomePageViewState extends State<HomePageView> {
       side: const BorderSide(width: 2.0, color: AppColors.aztecGold),
       backgroundColor: AppColors.darkGoldenrodMap[50],
       elevation: 10.0,
-      shadowColor: AppColors.darkGoldenrodMap[100]
-  );
+      shadowColor: AppColors.darkGoldenrodMap[100]);
+
+  final TextStyle titleTextStyle = TextStyle(
+      fontSize: 25.0,
+      fontWeight: FontWeight.w800,
+      color: AppColors.floralWhite);
 
   final TextStyle benefitTextStyle = TextStyle(
       fontSize: 20.0,
       fontWeight: FontWeight.w400,
-      color: AppColors.darkGoldenrodMap[700]
-  );
+      color: AppColors.darkGoldenrodMap[700]);
 }
